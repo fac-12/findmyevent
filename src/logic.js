3690c93607552e31fac4490453a45ddfae0a6460
Caroline <carolinelappleby@gmail.com>
2017-11-23 12:32:35 +0000

    did postcode filter array

--- src/logic.js
+++ src/logic.js
@@ -1,19 +1,23 @@
 const request = require('request');
 // const env = require('env2')('./../env');
 // const postCode = userInput;
+const EVENTFUL_API ='z5cBwm3jP9DZ6fkj';
+const postcode = 'e2%208dp';
+const category = 'comedy';
 
-
-const apiCall = (module, postcode, category) => {
-module('http://api.eventful.com/json/events/search?...&where='+postcode+'&category='+category'&within=5&app_key=' + EVENTFUL_API, function (error, response, body) {
+const apiCall = (module) => {
+module('http://api.eventful.com/json/events/search?...&where='+postcode+'&category='+category+'&within=5&app_key=' + EVENTFUL_API, function (error, response, body) {
     console.log('error:', error);
     console.log('statusCode:', response && response.statusCode);
     let results = JSON.parse(body);
-    console.log(results.events.event);
+    filterByPostCode(results.events.event);
   });
 };
 
 apiCall(request);
 
-// const filter = () => {
-//
-// }
+const filterByPostCode = (events) => {
+let postCodes = events.map(a => a.postal_code);
+   console.log(postCodes);
+
+}
